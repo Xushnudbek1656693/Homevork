e2617658675e07d003369279b217cc17b99551f7
Xushnudbek1656693 <xushnudbek1656@gmail.com>
2024-11-10 21:08:03 +0500

    foydalanuvchi haqida malumot oladi

---
+++ Dars3Aralash.py
@@ -0,0 +1,33 @@
+# 1
+while True:
+    def profil(ism = '', yosh = None, t_raqami = None):
+        pro ={
+            'Ism':ism,
+            'Yosh':yosh,
+            'T_raqam':t_raqami
+        }
+        return pro 
+    
+    oz = 0
+    ism = input("Ismingizni kiriting; ")
+    yosh = input("Yoshingizni kiriting; ")
+    t_raqam = input("Telefon raqamingizni kiriting; ")
+
+    try:
+        yosh = int(yosh)
+        t_raqam = int(t_raqam)
+    except:
+        oz = 1
+
+    if oz == 1:
+        print("INt tipidagi malumot kiriting")
+        continue
+    # global Foydalanuvchi
+    Foydalanuvchi = (ism,yosh,t_raqam)
+    for i in Foydalanuvchi:
+        if i == "":
+            oz = 1
+    if oz == 1:
+        print("Ismingizni kiritmadingiz ")
+        continue
+    break
